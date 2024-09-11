3ba0bf8983486179df622bb4ce2c1787577b433e
motoish <yuan.zhang@gmx.com>
2024-09-11 12:48:56 +0900

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,11 @@
+const { web } = require("projen");
+const project = new web.ReactProject({
+  defaultReleaseBranch: "main",
+  name: "roles-controller",
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
